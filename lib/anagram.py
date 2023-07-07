ded40a1d200eb68ed8c17f6e68db25edf107f777
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-07 08:45:29 -0700

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+words = ['enlists', 'google', 'inlets', 'banana', 'wow']
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, r):
+        match = []
+        split_word_input = sorted(list(r))
+
+        for word in words:
+            if sorted(word) == split_word_input:
+                match.append(word)
+        return match
+
+
+anagram = Anagram("word")
+result = anagram.match("letsin")
+print(result)
